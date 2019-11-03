a3bac77718b78dd29c467f28286a639120a6d188
tomocy <tomocy.dev@gmail.com>
2019-11-04 00:45:27 +0900

    add (*InJSON).PrintIndex
    (*inJSON).PrintIndex prints the index of entries in JSON.

--- cmd/rfv/format/format.go
+++ cmd/rfv/format/format.go
@@ -1,3 +1,14 @@
 package format
 
+import (
+	"encoding/json"
+	"io"
+
+	"github.com/tomocy/rfv/domain"
+)
+
 type InJSON struct{}
+
+func (p *InJSON) PrintIndex(w io.Writer, es []domain.Entry) {
+	json.NewEncoder(w).Encode(es)
+}
