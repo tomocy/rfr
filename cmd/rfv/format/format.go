7ecf8dcbf6dc1a0c85ae542613bae6afa95bb35b
tomocy <tomocy.dev@gmail.com>
2019-11-04 00:46:18 +0900

    add (*InJSON).Print
    (*InJSON).Print prints the given entry in JSON.

--- cmd/rfv/format/format.go
+++ cmd/rfv/format/format.go
@@ -12,3 +12,7 @@ type InJSON struct{}
 func (p *InJSON) PrintIndex(w io.Writer, es []domain.Entry) {
 	json.NewEncoder(w).Encode(es)
 }
+
+func (p *InJSON) Print(w io.Writer, e *domain.Entry) {
+	json.NewEncoder(w).Encode(e)
+}
