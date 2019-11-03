1088a7a58ae2cb2339b2426828eb225f56f3b8b5
tomocy <tomocy.dev@gmail.com>
2019-11-03 23:38:21 +0900

    add NewEntryUsecase
    NewEntryUsecase generates new usecase of entries.

--- app/usecase.go
+++ app/usecase.go
@@ -1,6 +1,14 @@
 package app
 
-import "github.com/tomocy/rfv/domain"
+import (
+	"github.com/tomocy/rfv/domain"
+)
+
+func NewEntryUsecase(repo domain.EntryRepo) *EntryUsecase {
+	return &EntryUsecase{
+		repo: repo,
+	}
+}
 
 type EntryUsecase struct {
 	repo domain.EntryRepo
