e4c577fcd9b014898b9924d2ef454bda7c93776d
tomocy <tomocy.dev@gmail.com>
2019-11-03 23:57:03 +0900

    add (*rfc).adapt
    (*rfc).adapt adapts itself to entry.

--- infra/infra.go
+++ infra/infra.go
@@ -2,6 +2,7 @@ package infra
 
 import (
 	"github.com/tomocy/rff"
+	"github.com/tomocy/rfv/domain"
 )
 
 type ViaHTTP struct{}
@@ -9,3 +10,9 @@ type ViaHTTP struct{}
 type index rff.Index
 
 type rfc rff.RFC
+
+func (r *rfc) adapt() *domain.Entry {
+	return &domain.Entry{
+		Title: r.Front.Title,
+	}
+}
