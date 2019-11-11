85285ef277c5498260b98cd8bcfc8bdf3608fa26
tomocy <tomocy.dev@gmail.com>
2019-11-11 18:19:22 +0900

    add (*Client).FetchIndex
    (*Client).FetchIndex fetches the index of RFCs.

--- infra/rfc/rfc.go
+++ infra/rfc/rfc.go
@@ -2,11 +2,28 @@ package rfc
 
 import (
 	"context"
+	"encoding/xml"
+	"fmt"
 	"io"
 )
 
 type Client struct {
 	Fetcher Fetcher
+}
+
+func (c *Client) FetchIndex(ctx context.Context) (*Index, error) {
+	fetched, err := c.Fetcher.Fetch(ctx, "//www.rfc-editor.org/rfc-index.xml")
+	if err != nil {
+		return nil, err
+	}
+	defer fetched.Close()
+
+	index := new(Index)
+	if err := xml.NewDecoder(fetched).Decode(index); err != nil {
+		return nil, fmt.Errorf("failed to decode: %s", err)
+	}
+
+	return index, nil
 }
 
 type Fetcher interface {
