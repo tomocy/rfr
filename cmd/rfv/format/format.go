472bfcd23ec6941fa4efe6c86c0bcde75cd9cf7f
tomocy <tomocy.dev@gmail.com>
2019-11-13 11:25:16 +0900

    modify (*InJSON).PrintIndex and (*InJSON).Print
    (*InJSON).PrintIndex and (*InJSON).Print prints entries which is converted from entries from domain.

--- cmd/rfv/format/format.go
+++ cmd/rfv/format/format.go
@@ -3,6 +3,7 @@ package format
 import (
 	"encoding/json"
 	"io"
+	"log"
 
 	"github.com/tomocy/rfv/domain"
 )
@@ -10,11 +11,18 @@ import (
 type InJSON struct{}
 
 func (p *InJSON) PrintIndex(w io.Writer, es []domain.Entry) {
-	json.NewEncoder(w).Encode(es)
+	converted := make([]entry, len(es))
+	for i, e := range es {
+		converted[i] = *convert(&e)
+	}
+
+	if err := json.NewEncoder(w).Encode(converted); err != nil {
+		log.Println(err)
+	}
 }
 
 func (p *InJSON) Print(w io.Writer, e *domain.Entry) {
-	json.NewEncoder(w).Encode(e)
+	json.NewEncoder(w).Encode(convert(e))
 }
 
 func convert(e *domain.Entry) *entry {
