b0d02e97517992a4e7970de28ff633abc9daddfb
tomocy <tomocy.dev@gmail.com>
2019-11-16 23:43:39 +0900

    update (*InHTML).Find
    (*InHTML).Find scrapes the title of the sections of the RFC of the given id.

--- infra/rfc/rfceditor/html/html.go
+++ infra/rfc/rfceditor/html/html.go
@@ -18,9 +18,26 @@ func (s *Old) Scrape(doc *goquery.Document) (*rfc.RFC, error) {
 	}
 
 	return &rfc.RFC{
-		ID:    id,
-		Title: doc.Find("span.h1").First().Text(),
+		ID:       id,
+		Title:    doc.Find("span.h1").First().Text(),
+		Sections: s.scrapeSections(doc),
 	}, nil
+}
+
+func (s *Old) scrapeSections(doc *goquery.Document) []*rfc.Section {
+	var secs []*rfc.Section
+	doc.Find("span.h2").Each(func(i int, s *goquery.Selection) {
+		title := strings.TrimLeft(s.Children().Remove().End().Text(), ". ")
+		if title == "" {
+			return
+		}
+
+		secs = append(secs, &rfc.Section{
+			Title: title,
+		})
+	})
+
+	return secs
 }
 
 type New struct{}
@@ -32,7 +49,24 @@ func (s *New) Scrape(doc *goquery.Document) (*rfc.RFC, error) {
 	}
 
 	return &rfc.RFC{
-		ID:    id,
-		Title: doc.Find("#title").Text(),
+		ID:       id,
+		Title:    doc.Find("#title").Text(),
+		Sections: s.scrapeSections(doc),
 	}, nil
+}
+
+func (s *New) scrapeSections(doc *goquery.Document) []*rfc.Section {
+	var secs []*rfc.Section
+	doc.Find("section[id^=section-]").Each(func(i int, s *goquery.Selection) {
+		title := s.Find("h2 a").Not(".section-number").First().Text()
+		if title == "" {
+			return
+		}
+
+		secs = append(secs, &rfc.Section{
+			Title: title,
+		})
+	})
+
+	return secs
 }
